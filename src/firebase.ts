334bfe1d374681136ffa0c9b5911161d9fef2725
Geoff <geoff.ijamieson@gmail.com>
2023-06-15 19:55:31 -0400

    switched firebase config to using env vars

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC9eUql_oU5kqs4I5_BhkWMxK35vyyPOHM",
-  authDomain: "vite-firebase-todo.firebaseapp.com",
-  projectId: "vite-firebase-todo",
-  storageBucket: "vite-firebase-todo.appspot.com",
-  messagingSenderId: "52108179158",
-  appId: "1:52108179158:web:c56fd404a861650a553b77",
-  measurementId: "G-7W6FGMDN10",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
